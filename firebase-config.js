86a373ede40c12886e5e96d3e9d922e207785cdf
gksiddalingaswamy <gksiddalingaswamy@gmail.com>
2026-09-15 09:58:14 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCtREvVL-QgP0umyKu-DiZUEynDECxnYaY",
+  authDomain: "malnadu-cafe-menu.firebaseapp.com",
+  databaseURL: "https://malnadu-cafe-menu-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "malnadu-cafe-menu",
+  storageBucket: "malnadu-cafe-menu.firebasestorage.app",
+  messagingSenderId: "1062877731937",
+  appId: "1:1062877731937:web:e27b853e43cbb7c3269e20"
+};
